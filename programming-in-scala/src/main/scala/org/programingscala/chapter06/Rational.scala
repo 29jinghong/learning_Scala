2753e06705746aa7b88c6e717459c3386eb3c24d
jinghong wang <29jinghong@gmail.com>
2024-05-06 18:01:08 -0400

    adding to ch6

--- programming-in-scala/src/main/scala/org/programingscala/chapter06/Rational.scala
+++ programming-in-scala/src/main/scala/org/programingscala/chapter06/Rational.scala
@@ -15,7 +15,6 @@ case class Rational(numer: Int, denom: Int)
     def gcf(a: Int, b: Int): Int = if (b == 0) a else gcf(b, a % b)
     //println(gcd(6, 12))
     
-    //need a function to simplyfy it 3/5 + 2/3 = 19/15
     def +(that: Rational): Rational = {
         val nNumer = numer * that.denom + that.numer * denom
         val nDenom = denom * that.denom
@@ -28,7 +27,7 @@ case class Rational(numer: Int, denom: Int)
         new Rational(numer * that.numer, denom * that.denom)
 
     def lessThan(that: Rational) =
-        this.numer * that.denom < that.numer * this.denom
+        this.numer / this.denom < that.numer / that.denom
 
     def max(that: Rational) =
         if (this.lessThan(that)) that else this
