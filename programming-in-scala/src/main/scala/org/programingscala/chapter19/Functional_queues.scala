5f5a04b134dae471403c98cc1f0c89f2121e33eb
29jinghong <29jinghong@gmail.com>
2025-01-15 12:40:56 -0500

    finishing 19.1

--- programming-in-scala/src/main/scala/org/programingscala/chapter19/Functional_queues.scala
+++ programming-in-scala/src/main/scala/org/programingscala/chapter19/Functional_queues.scala
@@ -7,17 +7,35 @@ class Functional_queues {
   val q1 = q append 4
 }
 
+//Big O notation
 class Queue1[T](elems: List[T]) {
   def head = elems.head
+  //head would have a complexity of O(1)
   def tail = elems.tail
   def append(x: T) = new Queue1(elems ::: List(x))
+  //append would have a complexity of O(n)
+  //n here is how many input are made^
+  //because how append would work here would be taking elems apart and then adding List(x) to the last
+  //which would look like this
+  //1,2,3,4 + 5
+  //2,3,4 + 5
+  //3,4 +5
+  //4 + 5
+  //3 + 4,5
+  //2 + 3,4,5
+  //1 + 2,3,4,5
 }
 
 class Queue2[T](smele: List[T]) {
   def head = smele.last
+  //head would have a complexity of O(n)
+  //because you are getting the last thing in the list so you would need to
+  //pop every thing from the top first and then you are able to access the last
   def tail = smele.init
   def append(x: T) = new Queue2(x :: smele)
+  //append would have a complexity of O(1)
 }
+
 
 class Queue[T](
                 private val leading: List[T],
