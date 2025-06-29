b87a0d0b0729b7568fd418dd45447d60371b8441
29jinghong <29jinghong@gmail.com>
2025-06-29 08:04:43 -0400

    finishing chapter 23

---
+++ src/main/scala/org/programingscala/chapter23/Chapter23.scala
@@ -0,0 +1,72 @@
+package org.programingscala.chapter23
+
+object Chapter23 {
+  def main(args: Array[String]): Unit = {
+    //23.1
+    case class Person(name: String,
+                      isMale: Boolean,
+                      children: Person*)
+
+    val lara = Person("Lara", false)
+    val bob = Person("Bob", true)
+    val julie = Person("Julie", false, lara, bob)
+
+    val persons = List(lara, bob, julie)
+
+    //
+    val motherChildPair =
+      for {
+        p <- persons if ! p.isMale
+        c <- p.children
+      } yield (p.name, c.name)
+
+    //you can also write this with filter and flatmap
+    val motherChildPair2 =
+      persons
+        .filter { p => ! p.isMale }
+        .flatMap {
+          p => p.children.map {
+            c => (p.name, c.name)
+          }
+        }
+
+    println(motherChildPair2)
+    println()
+    println(motherChildPair)
+
+
+    motherChildPair foreach println
+
+    val bobName =
+      for {
+        p <- persons if (p.name startsWith "Bo") // a generator that iterates over all elements with a filter
+      } yield {
+        p.name
+      }
+
+    println(bob)
+
+    val num =
+      for {
+        x <- List(1, 2)
+        y <- List("one", "two")
+        z <- List("a", "b")
+      } yield {
+        (x,y,z)
+      }
+
+    println(num)
+
+    val i1: Option[Int] = Some(1)
+    val i2: Option[Int] = Some(2)
+    val i3: Option[Int] = None
+
+    val sum =
+      for{
+        x <- i1
+        y <- i3
+      } yield x + y
+
+    println(sum)
+  }
+}
---
+++ src/main/scala/org/programingscala/chapter23/NQueen.scala
@@ -0,0 +1,8 @@
+package org.programingscala.chapter23
+
+object NQueen {
+  def main(args: Array[String]): Unit = {
+    //would need to figure out how to add 4 diferent lables for each row and print out the out come for each queen
+
+  }
+}
